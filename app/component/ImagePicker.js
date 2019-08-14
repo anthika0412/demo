5e7531d56046c2a37df94f54fe58a34728e5f10b
anthika.bluebik <anthika@bluebik.com>
2019-08-14 18:57:31 +0700

    display image from camera roll

--- app/component/ImagePicker.js
+++ app/component/ImagePicker.js
@@ -44,10 +44,10 @@ export default class ImagePickerScreen extends Component {
             CameraRoll.getPhotos({ ...DEFAULT_OPTIONS }).then((response) => {
                 this.setState({ refresh: false }, () => {
                     console.log("getPhotos response:", response)
-                    // this.setState({
-                    //     photos: response.edges,
-                    //     hasNext: response.page_info.has_next_page
-                    // })
+                    this.setState({
+                        photos: response.edges,
+                        hasNext: response.page_info.has_next_page
+                    })
                 })
             }).catch((error) => {
                 this.setState({ refresh: false })
@@ -88,15 +88,15 @@ export default class ImagePickerScreen extends Component {
                     renderItem={({item, index}) => {
                         return <TouchableOpacity onPress={() => this.onSelectedPhoto(item)}>
                             <Image local key={item.node.image.uri}
-                                uri={item.node.image.uri}
+                                source={{ uri: item.node.image.uri}}
                                 style={{
                                     width: Layout.width / 3,
                                     height: Layout.width / 3
                                 }} />
                             </TouchableOpacity>
                     }}
-                    // onEndReached={this.getMorePhotos}
-                    // onRefresh={this.getPhotos}
+                    onEndReached={this.getMorePhotos}
+                    onRefresh={this.getPhotos}
                     refreshing={this.state.refresh}
                     ListFooterComponent={this.state.hasNext ? <ActivityIndicator style={{ alignSelf: "center" }} /> : null} />
             </Modal>
